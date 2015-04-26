87d0f524053a4f01b24f7b70727bbc32b8f59fac
temdisponivel <temdisponivel@gmail.com>
2015-04-26 20:52:50 -0300

    Grid com scroll
    Ao adicionar linha ou coluna na grid ela cria o scroll e se mantém no
    tamanho padrão

--- python/interface_usuario.py
+++ python/interface_usuario.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import wx
 import wx.grid as wxgrid
+import wx.lib.scrolledpanel as scrolledpanel
 
 
 #classe do programa, contem os controles de tela, a janela, etc. Herda de wx.Frame
@@ -16,21 +17,28 @@ class Interface(wx.Frame):
         self.app = wx.App()
 
         #chama o contrutor do pai - wx.Frame
-        wx.Frame.__init__(self, None, -1, "Sensor Sônico", (0, 0), (Interface.LARGURA_TELA, Interface.ALTURA_TELA))
+        wx.Frame.__init__(self, None, 0, "Sensor Sônico", (0, 0), (Interface.LARGURA_TELA, Interface.ALTURA_TELA))
+
+        self.SetBackgroundColour("white")
 
         #cria botão de iniciar e finalizar
-        self.btn_iniciar = wx.Button(self, 0, "Iniciar", (10, 10), (Interface.LARGURA_BOTOES, Interface.ALTURA_BOTOES))
-        self.btn_finalizar = wx.Button(self, 1, "Finalizar", (10, 40), (Interface.LARGURA_BOTOES, Interface.ALTURA_BOTOES))
+        self.btn_iniciar = wx.Button(self, 2, "Iniciar", (10, 10), (Interface.LARGURA_BOTOES, Interface.ALTURA_BOTOES))
+        self.btn_finalizar = wx.Button(self, 3, "Finalizar", (10, 40), (Interface.LARGURA_BOTOES, Interface.ALTURA_BOTOES))
 
-        #cria a grid para mostrar os dados da leitura mostrando no Frame
-        self.grid_dados = wxgrid.Grid(self)
-        self.grid_dados.CreateGrid(15, 8)
+        #cria o panel onde ficará a grid
+        self.panel_grid = scrolledpanel.ScrolledPanel(self, 1, (10, 70), (100, 300))
+
+        #cria a grid para mostrar os dados da leitura mostrando no panel
+        self.grid_dados = wxgrid.Grid(self.panel_grid)
+        self.grid_dados.CreateGrid(10, 4)
 
         #cria um sizer vertical para a grid
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer_grid = wx.BoxSizer(wx.VERTICAL)
 
-        #adiciona a grid criado nosizer
-        self.sizer.Add(self.grid_dados, 1, wx.EXPAND)
+        #adiciona a grid criada no sizer
+        self.sizer_grid.Add(self.grid_dados)
+        self.panel_grid.SetSizerAndFit(self.sizer_grid)
+        self.grid_dados.AppendCols(2)
 
         #mostra a tela e entra no loop de eventos do aplicativo
         self.Show(True)
