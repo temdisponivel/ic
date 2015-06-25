83ac8b5324d39e0c8b97f58af934319fab8ea322
temdisponivel <temdisponivel@gmail.com>
2015-06-24 23:55:52 -0300

    Ajustes na interface e checkbox de leitura automática
    Adicionar o checkbox para inicio de leitura automática + ajustes na
    interface de usuário.

--- python/interface_usuario.py
+++ python/interface_usuario.py
@@ -26,13 +26,13 @@ class DadoLeituraVelocidadeAceleracao(Leitor.DadoLeitura):
         return super(DadoLeituraVelocidadeAceleracao, self).__str__() + " | Posição CM: " + str(self.posicao_cm) + " | Velocidade: " + str(self.velocidade) + " | Aceleração: " + str(self.aceleracao)
 
     def get_csv(self):
-        return super(DadoLeituraVelocidadeAceleracao, self).get_csv() + "," + str(self.posicao_cm) + "," + str(self.posicao_cm) + "," + str(self.velocidade) + "," + str(self.aceleracao)
+        return super(DadoLeituraVelocidadeAceleracao, self).get_csv() + "," + str(self.posicao_cm) + "," + str(self.velocidade) + "," + str(self.aceleracao)
 
 #classe que representa os graficos que sao mostrados na tela
 class Grafico(wx.Panel):
 
-    altura_grafico = 250
-    largura_grafico = 750
+    altura_grafico = 400
+    largura_grafico = 500
 
     def __init__(self, pai, posicao):
         wx.Panel.__init__(self, pai, -1, posicao, size=(Grafico.largura_grafico, Grafico.altura_grafico))
@@ -43,6 +43,8 @@ class Grafico(wx.Panel):
         #cria um canvas para imagem
         self.canvas = FigureCanvas(self, -1, self.figura)
 
+        self.figura.set_canvas(self.canvas)
+
         #cria um só plot
         self.eixos = self.figura.add_subplot(111)
 
@@ -52,6 +54,10 @@ class Grafico(wx.Panel):
         #adiciona os pontos x e y no grafico
         self.eixos.plot(pontosX, pontosY)
         self.canvas.draw()
+
+    def limpa(self):
+        #self.figura.
+        pass
 
 
 
@@ -66,6 +72,8 @@ class Interface(wx.Frame, Leitor.RecebeLeitura):
     def __init__(self):
         #cria o aplicativo
         self.app = wx.App()
+
+        self.app.SetAppName("Sensor Sônico")
 
         #chama o contrutor dos pais
         wx.Frame.__init__(self, None, 0, "Sensor Sônico", (0, 0), (Interface.largura_tela, Interface.altura_tela))
@@ -99,16 +107,20 @@ class Interface(wx.Frame, Leitor.RecebeLeitura):
 
         #cria botão de iniciar e finalizar
         self.btn_iniciar = wx.Button(self, 2, "Iniciar", (10, 10), (Interface.largura_botoes, Interface.altura_botoes))
-        self.btn_finalizar = wx.Button(self, 3, "Finalizar", (20 + Interface.largura_botoes, 10), (Interface.largura_botoes, Interface.altura_botoes))
+        self.btn_finalizar = wx.Button(self, 3, "Finalizar", (10, Interface.altura_botoes + 15), (Interface.largura_botoes, Interface.altura_botoes))
         self.Bind(wx.EVT_BUTTON, self.on_iniciar, id=2)
         self.Bind(wx.EVT_BUTTON, self.on_finalizar, id=3)
+
+        #cria check box para inicio automático de leitura
+        self.chk_inicio_automatico = wx.CheckBox(self, 4, "Inicio automático (inicia no movimento).", (Interface.largura_botoes + 20, 15))
+        self.Bind(wx.EVT_CHECKBOX, self.on_chk_inicio_automatico, id=4)
 
         #cria o panel onde ficará a grid
         self.panel_grid = scrolledpanel.ScrolledPanel(self, -1, (10, 70), (200, 300))
 
         #cria os graficos
-        self.grafico_velocidade = Grafico(self, (500, 10))
-        self.grafico_aceleracao = Grafico(self, (500, Grafico.altura_grafico + 10))
+        self.grafico_velocidade = Grafico(self, (425, 10))
+        self.grafico_aceleracao = Grafico(self, (425, Grafico.altura_grafico + 20))
 
         #cria a grid para mostrar os dados da leitura mostrando no panel
         self.grid_dados = wxgrid.Grid(self.panel_grid)
@@ -131,70 +143,129 @@ class Interface(wx.Frame, Leitor.RecebeLeitura):
         self.grafico_velocidade.SetSizerAndFit(self.sizer_velocidade)
         self.grafico_aceleracao.SetSizerAndFit(self.sizer_aceleracao)
 
+        #define valores iniciais
         self.arquivo = None
         self.arquivo_csv = None
+        self.automatico = False
+        self.dado_inicial = None
 
         #conecta o evento de fechar a janela a função
         self.Bind(wx.EVT_CLOSE, self.on_close)
 
+    def valida_leitura(self, dado):
+        if self.automatico:
+            if self.dado_inicial == None:
+                self.dado_inicial = dado
+            else:
+                return not self.dado_inicial == dado
+        else:
+            return True
+
+    #função que limpa os dados da tela
+    def limpa(self):
+        if (self.grid_dados.NumberRows > 0):
+            self.grid_dados.DeleteRows(0, self.grid_dados.GetNumberRows())
+
+        self.grafico_velocidade.limpa()
+        self.grafico_aceleracao.limpa()
+
+    #evento do check box de inicio automatico
+    def on_chk_inicio_automatico(self, event):
+        self.automatico = not self.automatico
+        self.chk_inicio_automatico.SetValue(self.automatico)
+
+        if self.automatico:
+            self.on_iniciar(event)
+            self.dado_inicial = None
+
     #Evento do botao iniciar
     def on_iniciar(self, event):
+        #limpa dados da tela
+        self.limpa()
         self.StatusBar.SetStatusText("Lendo dados da porta serial...")
+
+        #inicia leitura
         self.leitor.inicia()
+
+        #abre arquivos para escrita
         self.arquivo = open(self.arquivo_gravacao, "w")
         self.arquivo_csv = open(self.arquivo_gravacao_csv, "w")
+
+        #escreve cabeçalho das tabelas
+        self.arquivo_csv.write("Tempo,Dado Arduino,Posição CM,Velocidade,Aceleração \r")
 
     #Evento do botão finalizar
     def on_finalizar(self, event):
         self.StatusBar.SetStatusText("Ok")
+
+        #finaliza leitura do arduino
         self.leitor.finaliza()
 
-        if not self.arquivo == None:
+        #fecha arquivos
+        if (self.arquivo != None):
             self.arquivo.close()
-
-        if not self.arquivo_csv == None:
+        if (self.arquivo_csv != None):
             self.arquivo_csv.close()
 
+        #habilita novamento o checkbox
+        self.chk_inicio_automatico.Enable()
+
+    #processa os dados lidos do arduino
     def receber(self, dado_leitura):
-        #calcula os dados
+
+        #calcula os dados e valida se é um número válido
         try:
            informacoes = float(dado_leitura.dado)
-
            informacoes = DadoLeituraVelocidadeAceleracao(informacoes, dado_leitura.tempo)
         except Exception:
             return None
 
+        #printa as informacoes no console
         print(informacoes)
 
-        if (not self.arquivo.closed):
-            self.arquivo.write(informacoes.__str__() + '\r')
+        #valida se devemos processar as infomacoes (validacao necessaria por causa da leitura automatica)
+        if not self.valida_leitura(informacoes):
+            return None
+        else:
+            #se vamos ler, podemos definir a leitura automatica como false e desabilita o checkbox durante leitura
+            self.automatico = False
+            self.chk_inicio_automatico.Disable()
 
-        if (not self.arquivo_csv.closed):
+        #escreve informações nos arquivos
+        if (self.arquivo != None and not self.arquivo.closed):
+            self.arquivo.write(informacoes.__str__() + '\r')
+        if (self.arquivo_csv != None and not self.arquivo_csv.closed):
             self.arquivo_csv.write(informacoes.get_csv() + '\r')
 
+        #adiciona a linha na grid
         self.grid_dados.AppendRows()
         self.grid_dados.SetCellValue(self.grid_dados.GetNumberRows()-1, 0, str(informacoes.tempo))
         self.grid_dados.SetCellValue(self.grid_dados.GetNumberRows()-1, 1, str(informacoes.posicao_cm))
         self.grid_dados.SetCellValue(self.grid_dados.GetNumberRows()-1, 2, str(informacoes.velocidade))
         self.grid_dados.SetCellValue(self.grid_dados.GetNumberRows()-1, 3, str(informacoes.aceleracao))
 
+        #concatena as informacoes no vetor de informacoes
         self.informacoes_leitura.append(informacoes)
 
         velocidades = []
         tempos = []
         posicoes = []
 
+        #separa as velocidades, tempos e posicoes de todas as leituras
         for informacao in self.informacoes_leitura:
             tempos.append(informacao.tempo)
             posicoes.append(informacao.posicao_cm)
             velocidades.append(informacao.velocidade)
 
+        #printa tudo nos graficos
         self.grafico_velocidade.desenha(tempos, posicoes)
         self.grafico_aceleracao.desenha(tempos, velocidades)
 
+        #atualiza tamanho da grid desde que seja menor que a janela
         if (self.panel_grid.Size.GetHeight() < self.Size.GetHeight() - 150):
             self.panel_grid.SetSizerAndFit(self.sizer_grid)
 
+    #evento de fechar o programa
     def on_close(self, event):
         self.on_finalizar(event)
         self.app.Exit()
